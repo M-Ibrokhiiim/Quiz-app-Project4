6d293ca04fec43260abc9b3e92902ed6b07aebaf
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-03-05 15:41:23 +0300

    60% done

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ const questions=[
         question:"3.Where lives chinese people ?",
         options:["Chimgan","China","Norway","United Kingdom"],
         correctAswer:"China"
+    },
+    {
+        question:"4.Where lives chinese people ?",
+        options:["Chimgan","China","Norway","United Kingdom"],
+        correctAswer:"China"
+    },
+    {
+        question:"5.Where lives chinese people ?",
+        options:["Chimgan","China","Norway","United Kingdom"],
+        correctAswer:"China"
     }
 ]
 
@@ -25,25 +35,34 @@ const stratBtn=document.getElementById('startBtn')
 
 // UI give questions function
 
-let nextQuiz=0  
+let nextQuiz=1  
 let orderOfJson=0
-console.log(orderOfJson);
- 
+let existQuiz=questions[0]
 
-console.log(orderOfJson);
+
 // previousQuiz1()
-nextBtn.addEventListener("click",function previousQuiz1(){
-                
-     let increasing=nextQuiz++
-     
-    orderOfJson=increasing
-    console.log(orderOfJson);
+nextBtn.addEventListener("click",function previousQuiz1(){                
+       
+       let orderOfQuiz=nextQuiz++
+       let existQuiz=questions[orderOfQuiz]
+       let UI=`
+        <h6 class="quiz-section-quiz"> ${existQuiz.question}</h6>
+           <div class="quiz-section-option-div">
+             <button class="options"> ${existQuiz.options[0]}</button>
+             <button class="options">${existQuiz.options[1]}</button>
+             <button class="options">${existQuiz.options[2]}</button>
+             <button class="options">${existQuiz.options[3]}</button>
+           </div>
+     `
     
-    return orderOfJson
-})
+    
+       const appealToUIelements=document.getElementById('quiz-sectionId')
+       appealToUIelements.innerHTML=UI
+       
+       return orderOfJson
+    })
 
-let existQuiz=questions[orderOfJson]
-console.log(existQuiz);
+
 
 
 function startBtn(){
@@ -52,24 +71,25 @@ function startBtn(){
 stratBtn.style.display="none"
 nextBtn.style.display="block"
 
-
+     
 let UI=`
-     <h6 class="quiz-section-quiz"> ${existQuiz.question}</h6>
-        <div class="quiz-section-option-div">
-          <button class="options"> ${existQuiz.options[0]}</button>
-          <button class="options">${existQuiz.options[1]}</button>
-          <button class="options">${existQuiz.options[2]}</button>
-          <button class="options">${existQuiz.options[3]}</button>
-        </div>
-  `
-    
+    <h6 class="quiz-section-quiz"> ${existQuiz.question}</h6>
+       <div class="quiz-section-option-div">
+         <button class="options"> ${existQuiz.options[0]}</button>
+         <button class="options">${existQuiz.options[1]}</button>
+         <button class="options">${existQuiz.options[2]}</button>
+         <button class="options">${existQuiz.options[3]}</button>
+       </div>
+ `
 
-const appealToUIelements=document.getElementById('quiz-sectionId')
-appealToUIelements.innerHTML=UI
+   // console.log(orderOfJson);
+
+   const appealToUIelements=document.getElementById('quiz-sectionId')
+   appealToUIelements.innerHTML=UI
+   
+
 
 }
 
 
 
-
- 
