3842b69218627ff5fae6c4c7e58100a95a4dcc38
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-03-10 14:30:40 +0300

    100% done

--- index.js
+++ index.js
@@ -2,27 +2,33 @@ const questions=[
     {
         question:"1.What is the largest animal in the World?",
         options:["Whale","Shark","Camel","Nortuya"],
+        correctOption:'Whale'
     },
     {
         question:"2.Which  country is the largest  in the world?",
-        options:["China","Russia","The USA","Georgia"]
+        options:["China","Russia","The USA","Georgia"],
+        correctOption:'China'
     },
     {
         question:"3.Where lives chinese people ?",
-        options:["Chimgan","China","Norway","United Kingdom"]
+        options:["Chimgan","China","Norway","United Kingdom"],
+        correctOption:'China'
     },
     {
         question:"4.Where  located chinese people of location ?",
-        options:["Chimgan"," North China","Norway","United Kingdom"]
+        options:["Chimgan"," North China","Norway","United Kingdom"],
+        correctOption:'North China'
     },
     {
         question:"5.Where lives uygur people ?",
-        options:["Chimgan","Uyguriston","Norway","United Kingdom"]
+        options:["Chimgan","Uyguriston","Norway","United Kingdom"],
+        correctOption:'Uyguriston'
     }
 ]
 
 
-const answers=["Whale","China","China","China","Uyguriston"]
+
+
 
 // Buttons
 
@@ -48,17 +54,29 @@ nextBtn.addEventListener("click",function previousQuiz1(){
        let orderOfQuiz=nextQuiz++
        let existQuiz=questions[orderOfQuiz]
        let UI=''
+
        if(orderOfQuiz===5){
         appealToUIelements.innerHTML=''
         nextBtn.style.display="none"
 
         for(let i=0; i < choseAnswers.length; i++){
-            if(choseAnswers[i]===answers[i]){
-                overallScore++
+            if(choseAnswers[i]===questions[i].correctOption){
+              overallScore++
             }
         }
-        console.log(overallScore);
+
+        UI=` 
+         <div id="totalAnswerPlace">
+         <h1>Your total score:${overallScore}/5</h1>
+         
         
+        <div class="restart-button-container">
+          <a class="restart-button" onclick="location.reload()"><i class="fa-solid fa-rotate-left reIcon"></i>Restart</a>
+        </div>
+         </div>
+         `
+        
+        appealToUIelements.innerHTML=UI
         return overallScore
        }
        
@@ -77,13 +95,8 @@ nextBtn.addEventListener("click",function previousQuiz1(){
 
         
      `
-         
-       
+       nextBtn.style.display='none'       
        appealToUIelements.innerHTML=UI
-       
-    //  Choosing answers function()
-    
-       
        return orderOfJson
     })
 
@@ -92,7 +105,7 @@ nextBtn.addEventListener("click",function previousQuiz1(){
 function startBtn(){
 
 stratBtn.style.display="none"
-nextBtn.style.display="block"
+
 
      
 let UI=`
@@ -109,30 +122,8 @@ let UI=`
 
    const appealToUIelements=document.getElementById('quiz-sectionId')
    appealToUIelements.innerHTML=UI
-   
-
-
 }
-
-
-
-
-// const appealToBtnContainer=document.getElementById('quiz-sectionId')
-// appealToBtnContainer.addEventListener('click',(event)=>{
-
-//  if(event.target.classList.contains('options'))
-//    const appealToParentElement=event.target.parentElement
-//    const btnText=appealToParentElement.querySelector('options').textContent
-//    console.log(btnText);
-   
-// })
-
-
-
-const buttons=document.querySelectorAll(' .quiz-section-option-div .options')
-
-
-
+ 
  
 const quizSection = document.getElementById('quiz-sectionId');
 
@@ -140,17 +131,12 @@ let valOfButton=''
 
 quizSection.addEventListener('click', (event) => {
 
-
     if (event.target.classList.contains('options')){
         let btnValue=event.target.textContent.trim()
         valOfButton+=btnValue
-        
         choseAnswers.push(valOfButton)
         valOfButton=''
+        nextBtn.style.display="block"
         return valOfButton
     }
-});
-
-console.log(choseAnswers);
-
-console.log(overallScore);
+}); 
